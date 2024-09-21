04128b66a0a3de0568605452e973a3485f8a0002
Denis Saveliev <denissaveliev23@gmail.com>
2024-09-22 00:03:41 +0300

    feat: create register api

--- api/src/users/UserController.ts
+++ api/src/users/UserController.ts
@@ -1,7 +1,37 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from 'express';
+import UserSchema from './UserSchema';
+import bcrypt from 'bcrypt';
 
 export const register = async (req: Request, res: Response, next: NextFunction) => {
-  res.json({ message: 'register' });
+  const { name, email, password } = req.body;
+
+  if (!name || !email || !password) {
+    return res.status(400).json({ error: 'All fields are required.' });
+  }
+
+  const user = await UserSchema.findOne({ email });
+
+  if (user) {
+    return res.status(400).json({ error: 'User already exist.' });
+  }
+
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await UserSchema.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return res.status(201).json({
+      status: true,
+      message: 'User created',
+      data: { _id: newUser._id, email: newUser.email },
+    });
+  } catch {
+    return res.status(500).json({ error: 'Something went wrong.' });
+  }
 };
 
 export const login = async (req: Request, res: Response, next: NextFunction) => {
