cb5d93ba8003e4583a8d35cf649724668a04085c
Denis Saveliev <denissaveliev23@gmail.com>
2024-09-26 10:39:05 +0300

    feat: add greeting

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { AuthService } from '../../core/sevices/auth.service';
+import { User } from '../../core/model/common.model';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,10 +9,20 @@ import { AuthService } from '../../core/sevices/auth.service';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
   private authService = inject(AuthService);
+  user!: User;
 
   logout() {
     this.authService.logout();
+  }
+
+  ngOnInit(): void {
+    this.authService.me().subscribe({
+      next: response => {
+        console.log(response);
+        this.user = response.data;
+      },
+    });
   }
 }
